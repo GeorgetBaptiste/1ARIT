2e1f5fa42cec2a71afc3b12a3579f1eb4a813a9f
Baptiste Georget <baptiste.georget@hotmail.com>
2022-04-28 17:19:42 +0200

    base de l'affichage

--- interface.py
+++ interface.py
@@ -11,23 +11,14 @@ class Interface:
         self.__canvas = Canvas(self.__root, width=screen_width, height=screen_height, highlightthickness=0, bg="red")
         self.__canvas.place(x=screen_width/2, y=screen_height/2, anchor="center")
         
-        self.__button_chiffrement_image = Button(self.__root, text="Chiffrement image", width=20, pady=5, highlightbackground="black")
-        self.__button_chiffrement_image.place(x=screen_width*0.15, y=screen_height*0.25, anchor="center")
+        self.__button_chiffrement_image = Button(self.__root, text="Chiffrement image", width=20, pady=50, padx=75, highlightbackground="black", font=("Helvetica", 30, "bold"))
+        self.__button_chiffrement_image.place(x=screen_width*0.5, y=screen_height*0.25, anchor="center")
         
-        self.__button_masquage_image = Button(self.__root, text="Masquage image", width=20, pady=5, highlightbackground="black")
-        self.__button_masquage_image.place(x=screen_width*0.15, y=screen_height*0.50, anchor="center")
+        self.__button_masquage_image = Button(self.__root, text="Masquage image", width=20, pady=50, padx=75, highlightbackground="black", font=("Helvetica", 30, "bold"))
+        self.__button_masquage_image.place(x=screen_width*0.5, y=screen_height*0.50, anchor="center")
         
-        self.__button_masquage_texte = Button(self.__root, text="Masquage texte", width=20, pady=5, highlightbackground="black")
-        self.__button_masquage_texte.place(x=screen_width*0.15, y=screen_height*0.75, anchor="center")
-        
-        self.__entry_chiffrement_image = Entry(self.__root, width=20, highlightthickness=4, relief=FLAT)
-        self.__entry_chiffrement_image.place(x=screen_width*0.3, y=screen_height*0.25, anchor="center")
-        
-        self.__entry_masquage_image = Entry(self.__root, width=20, highlightthickness=4, relief=FLAT)
-        self.__entry_masquage_image.place(x=screen_width*0.3, y=screen_height*0.50, anchor="center")
-        
-        self.__entry_masquage_texte = Entry(self.__root, width=20, highlightthickness=4, relief=FLAT)
-        self.__entry_masquage_texte.place(x=screen_width*0.3, y=screen_height*0.75, anchor="center")
+        self.__button_masquage_texte = Button(self.__root, text="Masquage texte", width=20, pady=50, padx=75, highlightbackground="black", font=("Helvetica", 30, "bold"))
+        self.__button_masquage_texte.place(x=screen_width*0.5, y=screen_height*0.75, anchor="center")
         
         self.__root.mainloop()
         
